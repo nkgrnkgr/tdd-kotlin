36738588d5236344597ca3affc7a48a51032d4f7
nkgrnkgr <beefupme@gmail.com>
2019-03-23 09:53:08 +0900

    finished chapter04

---
+++ src/main/kotlin/chapter04/money/Dollar.kt
@@ -0,0 +1,22 @@
+package chapter04.money
+
+class Dollar(private val amount: Int) {
+    fun times(multiplier: Int): Dollar {
+        return Dollar(amount * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dollar
+
+        if (amount != other.amount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return amount
+    }
+}
