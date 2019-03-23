2de6beaec80f19034ee92135dadc7b3c35ede3ba
nkgrnkgr <beefupme@gmail.com>
2019-03-23 13:03:17 +0900

    create commit.sh

---
+++ commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter chapter_number"
+read NUMBER
+
+git add .
+git commit -m "finished chapter$NUMBER"
+
+echo "Can I push to origin/master (y/n)"
+read C
+
+if $C = "y" || $C = "Y"; then
+  git ch master
+  git merge hotfix_$NUMBER
+  git push origin master
+else
+  git reset --hard HEAD^
+if
+
+
+
+git st
