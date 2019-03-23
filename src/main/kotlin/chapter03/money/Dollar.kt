6aea3d05505ce48876efc9fbe20488541a4ef4c9
nkgrnkgr <beefupme@gmail.com>
2019-03-23 09:37:58 +0900

    finished chapter3

--- src/main/kotlin/chapter03/money/Dollar.kt
+++ src/main/kotlin/chapter03/money/Dollar.kt
@@ -1,7 +1,22 @@
 package chapter03.money
 
-data class Dollar(val amount: Int) {
+class Dollar(val amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dollar
+
+        if (amount != other.amount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return amount
     }
 }
--- src/test/kotlin/chapter03/money/DollarTest.kt
+++ src/test/kotlin/chapter03/money/DollarTest.kt
@@ -1,6 +1,8 @@
 package chapter03.money
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class DollarTest {
@@ -13,5 +15,12 @@ internal class DollarTest {
         assertEquals(10, product.amount)
         val product1 = five.times(3)
         assertEquals(15, product1.amount)
+    }
+
+    @Test
+    internal fun testEquality() {
+        // Kotlin は data Class にすると equals と hasCode を自動で生成してくれるのでここではdataクラスではないものとする
+        assertTrue(Dollar(5).equals(Dollar(5)))
+        assertFalse(Dollar(5).equals(Dollar(6)))
     }
 }
