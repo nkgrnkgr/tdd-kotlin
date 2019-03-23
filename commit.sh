617e1e0022420d2610392e59b15d92d0202608bc
nkgrnkgr <beefupme@gmail.com>
2019-03-23 13:26:45 +0900

    fix

--- commit.sh
+++ commit.sh
@@ -9,7 +9,7 @@ git commit -m "finished chapter$NUMBER"
 echo "Can I push to origin/master (y/n)"
 read C
 
-if $C ="y" -o $C ="Y"; then
+if [ $C = "y" ]; then
   git ch master
   git merge hotfix_$NUMBER
   git push origin master
